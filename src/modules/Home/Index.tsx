2da679455fb241e2b3e005670e0384c5fd824e23
Tamby Tia RAZAFINDRALAMBO <tambytia.razafindralambo@elca.mu>
2025-08-13 12:48:51 +0400

    add icon

--- src/modules/Home/Index.tsx
+++ src/modules/Home/Index.tsx
@@ -1,15 +1,27 @@
+import ReactLogo from "@icons/react.svg?react";
 import useUserQuery from "@/hooks/useUserQuery";
-import React from "react";
+import React, { useState } from "react";
 
 const HomeScreen: React.FunctionComponent = () => {
   const { data, isLoading } = useUserQuery();
-
-  console.log(data);
+  const [count, setCount] = useState(0);
 
   return (
-    <div>
-      Home
-      <div>{isLoading ? "isLoading" : JSON.stringify(data?.body)}</div>
+    <div className="flex flex-col gap-3 items-center mx-auto">
+      <a
+        href="https://react.dev"
+        target="_blank"
+        className="inline-block w-full mb-10"
+      >
+        <ReactLogo className="logo-spin h-[100px] w-auto mx-auto" />
+      </a>
+      <h1>Vite + React</h1>
+      <button onClick={() => setCount((count) => count + 1)}>
+        count is {count}
+      </button>
+      <p className="text-center">
+        {isLoading ? "isLoading" : JSON.stringify(data?.body)}
+      </p>
     </div>
   );
 };
